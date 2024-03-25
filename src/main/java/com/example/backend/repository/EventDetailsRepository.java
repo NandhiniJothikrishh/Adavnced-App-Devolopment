2cf380a09ebb4ee6270cd888755597cd6ec984bc
NandhiniJothikrishh <126311373+NandhiniJothikrishh@users.noreply.github.com>
2024-03-25 16:24:33 +0530

    Delete src directory

--- src/main/java/com/example/backend/repository/EventDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.EventDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface EventDetailsRepository extends JpaRepository<EventDetails, Long> {
-}
--- src/main/java/com/example/backend/repository/PaymentDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-
-import com.example.backend.model.PaymentDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, Long> {
-}
